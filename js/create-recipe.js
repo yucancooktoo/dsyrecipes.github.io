2f272a3daf31eec9698cb949e02d330d143f1c65
yucancooktoo <97014806+yucancooktoo@users.noreply.github.com>
2022-06-02 09:23:22 -0400

    Update create-recipe.js

--- js/create-recipe.js
+++ js/create-recipe.js
@@ -69,6 +69,9 @@ $(document).ready(function() {
           section = linkify(section.replaceAll("'","asdfqwert").replaceAll("_","bsdfqwert").replaceAll("(","csdfqwert").replaceAll(")","dsdfqwert"))
           section = section.replaceAll("asdfqwert","'").replaceAll("bsdfqwert","_").replaceAll("csdfqwert","(").replaceAll("dsdfqwert",")");          
         }
+        //section = section.replace("notes","things to do");
+        //section = section.replace("steps","itinerary");
+        //section = section.replace("based on","references");
         
         // place the html inside its section
         $('#' + id).html(section);
